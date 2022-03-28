8fc529c2159fa53bce3ec16833ad9f4c5e7387f6
k98a73 <100816862+k98a73@users.noreply.github.com>
2022-03-28 15:29:13 +0900

    ユーザー管理機能の修正1

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,25 +2,25 @@ class ItemsController < ApplicationController
   def index
   end
 
-  def new
-  end
+  # def new
+  # end
 
-  def create
-    @item = Item.new(item_params)
-    if @item.save
-      redirect_to root_path
-    else
-      render :new
-    end
-  end
+  # def create
+  #   @item = Item.new(item_params)
+  #   if @item.save
+  #     redirect_to root_path
+  #   else
+  #     render :new
+  #   end
+  # end
 
-  def destroy
-  end
+  # def destroy
+  # end
 
-  private
+  # private
 
-  def item_params
-    params.require(:item).permit(:image, :item_name, :description, :category_id, :condition_id, :postage_id, :prefecture_id,
-                                 :days_to_ship_id, :price).merge(user_id: current_user.id)
-  end
+  # def item_params
+  #   params.require(:item).permit(:image, :item_name, :description, :category_id, :condition_id, :postage_id, :prefecture_id,
+  #                                :days_to_ship_id, :price).merge(user_id: current_user.id)
+  # end
 end
