3ec957adbe99c488b04fcde798f49b5312c961ad
k98a73 <100816862+k98a73@users.noreply.github.com>
2022-03-28 14:20:45 +0900

    ユーザー管理機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,26 @@
 class ItemsController < ApplicationController
-
   def index
-    
   end
 
+  def new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def destroy
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:image, :item_name, :description, :category_id, :condition_id, :postage_id, :prefecture_id,
+                                 :days_to_ship_id, :price).merge(user_id: current_user.id)
+  end
 end
