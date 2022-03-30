546a9de2cd22d19703124fe4f3dd079ae4951af2
k98a73 <100816862+k98a73@users.noreply.github.com>
2022-03-30 14:24:13 +0900

    商品出品機能の修正1

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -3,9 +3,9 @@ function post (){
     priceInput.addEventListener("input", () => {
       const inputValue = priceInput.value;
       const addTaxDom = document.getElementById("add-tax-price");
-      addTaxDom.innerHTML = Math.floor(priceInput.value *0.1 );
+      addTaxDom.innerHTML = Math.floor(inputValue *0.1 );
       const addProfitDom = document.getElementById("profit");
-      addProfitDom.innerHTML = Math.floor(priceInput.value *0.9 );
+      addProfitDom.innerHTML = Math.floor(inputValue *0.9 );
   }); 
  };
  
