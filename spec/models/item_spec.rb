d1d19612bcdbfcfe9948f4635b0823881f91a9c3
k98a73 <100816862+k98a73@users.noreply.github.com>
2022-03-30 17:03:36 +0900

    indexアクションの記述

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -29,27 +29,27 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include "Description can't be blank"
       end
       it 'category_idが空では出品できない' do
-        @item.category_id = '1'
+        @item.category_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include "Category can't be blank"
       end
       it 'condition_idが空では出品できない' do
-        @item.condition_id = '1'
+        @item.condition_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include "Condition can't be blank"
       end
       it 'postage_idが空では出品できない' do
-        @item.postage_id = '1'
+        @item.postage_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include "Postage can't be blank"
       end
       it 'prefecture_idが空では出品できない' do
-        @item.prefecture_id = '1'
+        @item.prefecture_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include "Prefecture can't be blank"
       end
       it 'days_to_ship_idが空では出品できない' do
-        @item.days_to_ship_id = '1'
+        @item.days_to_ship_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include "Days to ship can't be blank"
       end
@@ -59,12 +59,12 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include "Price can't be blank"
       end
       it 'priceが300未満では出品できない' do
-        @item.price = '299'
+        @item.price = 299
         @item.valid?
         expect(@item.errors.full_messages).to include 'Price is out of setting range'
       end
       it 'priceが9_999_999より多いと出品できない' do
-        @item.price = '10_000_000'
+        @item.price = 10_000_000
         @item.valid?
         expect(@item.errors.full_messages).to include 'Price is out of setting range'
       end
