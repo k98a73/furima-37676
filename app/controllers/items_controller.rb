8c1f8688daf05740bbbaf602d894b414ad673d29
k98a73 <100816862+k98a73@users.noreply.github.com>
2022-03-31 20:55:14 +0900

    Rubocopのチェック内容を修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,7 +3,7 @@ class ItemsController < ApplicationController
   before_action :set_item, except: [:index, :new, :create]
 
   def index
-    @items = Item.includes(:user).order("created_at DESC")
+    @items = Item.includes(:user).order('created_at DESC')
   end
 
   def new
@@ -31,5 +31,5 @@ class ItemsController < ApplicationController
 
   def set_item
     @item = Item.find(params[:id])
-  end 
+  end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -12,7 +12,9 @@ class Item < ApplicationRecord
     validates :image
     validates :item_name, length: { maximum: 40 }
     validates :description, length: { maximum: 1000 }
-    validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: 'is out of setting range' }
+    validates :price,
+              numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999,
+                              message: 'is out of setting range' }
   end
 
   with_options numericality: { other_than: 1, message: "can't be blank" } do
