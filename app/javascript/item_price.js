517757940360e0c24355ceef3e7469e01286a551
k98a73 <100816862+k98a73@users.noreply.github.com>
2022-04-01 15:12:37 +0900

    販売利益を切り上げに修正

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -5,7 +5,7 @@ function post (){
       const addTaxDom = document.getElementById("add-tax-price");
       addTaxDom.innerHTML = Math.floor(inputValue *0.1 );
       const addProfitDom = document.getElementById("profit");
-      addProfitDom.innerHTML = Math.floor(inputValue *0.9 );
+      addProfitDom.innerHTML = Math.ceil(inputValue *0.9 );
   }); 
  };
  
