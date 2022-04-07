3291576a19d82b5a6a5bc5a050f18262004e8fe9
k98a73 <100816862+k98a73@users.noreply.github.com>
2022-04-07 16:33:42 +0900

    Formオブジェクトパターンの実装

---
+++ app/models/record_address.rb
@@ -0,0 +1,23 @@
+class RecordAddress
+  include ActiveModel::Model
+  attr_accessor :post_code, :prefecture_id, :municipalities, :address, :building, :phone_number, :item_id, :user_id, :token
+
+  validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
+
+  with_options presence: true do
+    validates :post_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
+    validates :municipalities
+    validates :address
+    validates :phone_number, format: { with: /\A^(0{1}\d{9,10})$\z/, message: 'is invalid. Input only number' }
+    validates :item_id
+    validates :user_id
+    validates :token
+  end
+
+  def save
+    record = Record.create(item_id: item_id, user_id: user_id)
+
+    Address.create(post_code: post_code, prefecture_id: prefecture_id, municipalities: municipalities, address: address,
+                   building: building, phone_number: phone_number, record_id: record.id)
+  end
+end
